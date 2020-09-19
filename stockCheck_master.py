9e3aa78dd571d3e3143fb181a46d162a4ec77161
Jacob Tyndall <jacob.n.tyndall@gmail.com>
2020-09-19 04:02:26 -0500

    Fixed the actual parsing of the data being outside of the try block.

--- stockCheck_master.py
+++ stockCheck_master.py
@@ -4,7 +4,6 @@ import requests
 import json
 import re
 import time
-import webbrowser
 from fake_useragent import UserAgent
 from twilio.rest import Client
 
@@ -15,12 +14,10 @@ from dataclasses import dataclass
 #disable annoying warnings
 urllib3.disable_warnings(InsecureRequestWarning)
 
-url="https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/en_us/USD/5438481700"
-
-twi_realPhoneNumber = "xxxxxxxx"
-twi_twilio_account_sid = 'xxxxxxxx'
-twi_twilio_auth_token = 'xxxxxxxx'
-twi_twilio_virtualPhoneNumber = "xxxxxxxx"
+twi_realPhoneNumber = "xxxxxx"
+twi_twilio_account_sid = 'xxxxxx'
+twi_twilio_auth_token = 'xxxxxx'
+twi_twilio_virtualPhoneNumber = "xxxxxx"
 twi_client = Client(twi_twilio_account_sid, twi_twilio_auth_token)
 
 def sendTextMesage(messageBody):
@@ -44,11 +41,12 @@ class PollingTarget:
         while continuePolling:
             try:
                 siteData = http.request('GET', self.pollUrl,headers={"User-Agent": UserAgent().random}).data.decode('utf-8')
+                isInStock = self.pollFunc(siteData)
             except:
-                print("An error occured while parsing NVidia's data, trying again in a moment.")
+                print("An error occured while parsing " + self.name + ", trying again in a moment.")
                 time.sleep(2)
                 continue
-            if self.pollFunc(siteData):
+            if isInStock:
                 print(self.name + " IN STOCK!")
                 sendTextMesage(self.name + " IN STOCK. Purchase link:\n" + self.purchaseUrl)
                 break
@@ -60,28 +58,28 @@ allPollingTargets = [
     PollingTarget(
         name = "[NVIDIA] FE",
         pollUrl = "https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/en_us/USD/5438481700",
-        interval = 25,
+        interval = 10,
         purchaseUrl = "https://store.nvidia.com/store/nvidia/en_US/buy/productID.5438481700/clearCart.yes/nextPage.QuickBuyCartPage",
         pollFunc = lambda data : re.search('OUT_OF_STOCK', json.loads(data)["products"]["product"][0]["inventoryStatus"]["status"], re.IGNORECASE) == None
     ),
     PollingTarget(
         name = "[AMAZON] EVGA FTW3",
         pollUrl = "https://www.amazon.com/gp/aws/cart/add.html?ASIN.2=B08HR3DPGW&Quantity.2=1&ref_=nav_custrec_signin&confirmPage=confirm",
-        interval = 25,
+        interval = 10,
         purchaseUrl = "https://www.amazon.com/gp/aws/cart/add.html?ASIN.2=B08HR3DPGW&Quantity.2=1&ref_=nav_custrec_signin&confirmPage=confirm",
         pollFunc = lambda data : re.search('currently unavailable', data, re.IGNORECASE) == None
     ),
     PollingTarget(
         name = "[AMAZON] EVGA FTW3 ULTRA",
         pollUrl = "https://www.amazon.com/gp/aws/cart/add.html?ASIN.2=B08HR3Y5GQ&Quantity.2=1&ref_=nav_custrec_signin&confirmPage=confirm",
-        interval = 25,
+        interval = 10,
         purchaseUrl = "https://www.amazon.com/gp/aws/cart/add.html?ASIN.2=B08HR3Y5GQ&Quantity.2=1&ref_=nav_custrec_signin&confirmPage=confirm",
         pollFunc = lambda data : re.search('currently unavailable', data, re.IGNORECASE) == None
     ),
     PollingTarget(
         name = "[EVGA  ] EVGA FTW3",
         pollUrl = "https://www.evga.com/products/product.aspx?pn=10G-P5-3897-KR",
-        interval = 25,
+        interval = 10,
         purchaseUrl = "https://www.evga.com/products/product.aspx?pn=10G-P5-3897-KR",
         pollFunc = lambda data : re.search('out of stock', data, re.IGNORECASE) == None
     )
